b79eec077cebd00eb30cbbc40acf944da65010bc
biancavee15 <vasquezbmarie@gmail.com>
2018-03-13 11:25:10 -0400

    completed 6 knex exercises

--- drills.js
+++ drills.js
@@ -7,15 +7,53 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\x1Bc');
 
 // Sample select 
-knex
-  .select()
-  .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(results));
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
 
 
 // Destroy the connection pool
-knex.destroy().then(() => {
-  console.log('database connection closed');
-});
+// knex.destroy().then(() => {
+//   console.log('database connection closed');
+// });
+
+// knex
+//   .select()
+//   .from('restaurants')
+//   .debug(true)
+//   .then(results => console.log(results));
+// knex
+//   .select()
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .debug(true)
+//   .then(results => console.log(results));
+// knex 
+//   .select('id','name')
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .debug(true)
+//   .then(results => console.log(results));
+// knex
+//   .select()
+//   .from('restaurants')
+//   .where('cuisine', 'Thai')
+//   .count()
+//   .debug(true)
+//   .then(results => console.log(results));
+// knex
+//   .select()
+//   .from('restaurants')
+//   .count()
+//   .debug(true)
+//   .then(results => console.log(results));
+// knex
+//   .select()
+//   .from('restaurants')
+//   .where({'cuisine': 'Thai','address_zipcode': '11372'})
+//   .count()
+//   .debug(true)
+//   .then(results => console.log(results));
